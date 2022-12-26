1752a1738dd7943ba8d418db7bc04c9236637d92
josefiino <bernardo.tofani@hotmail.com>
2022-12-26 15:49:58 -0300

    Restructure folder

--- client/src/constants/apiUrls.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
-export const BOOKS_URL = "/books";
-export const PRODUCTS_URL = "/products";
-export const CATEGORIES_URL = "/categories";
-export const USERS_URL = "/users";
